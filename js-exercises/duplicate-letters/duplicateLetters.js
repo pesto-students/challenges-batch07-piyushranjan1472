21fc0eb1b96d7ee9f8de7db270feb13daf8b0b9f
piyush ranjan <prpiyushranjan05@gmail.com>
2021-02-17 11:34:53 +0530

    re-written using more features

--- js-exercises/duplicate-letters/duplicateLetters.js
+++ js-exercises/duplicate-letters/duplicateLetters.js
@@ -1,35 +1,25 @@
 function duplicateLetters(str) {
-  var counter = {};
-
-  for (var i = 0, len = str.length; i < len; i += 1) {
-    counter[str[i]] = (counter[str[i]] || 0) + 1;
-  }
-
-  var biggest = -1, number;
-  for (var key in counter) {
+  const counter = {};
+  for (const i of str) counter[i] = (counter[i] || 0) + 1;
+  let biggest = -1;
+  let number;
+  for (const key in counter) {
     if (counter[key] > biggest) {
       biggest = counter[key];
       number = key;
     }
   }
-
   let count = 0;
   // looping through the items
-  for (let i = 0; i < str.length; i++) {
-
-    // check if the character is at that position
-    if (str.charAt(i) == number) {
-      count += 1;
-    }
+  for (const i of str) {
+    if (i === number) count += 1;
   }
-
 
   if (count > 1) {
     return count;
   }
   return false;
 }
-
 
 export {
   duplicateLetters,
